f130780d0b17b491f02f24b9db66e8653cdff0b2
Haneenelasawy <Haneenelasawy335@gmail.com>
2026-03-27 10:32:32 +0200

    fix job list template , add job detail url ,and display job count dynamics

--- job/models.py
+++ job/models.py
@@ -17,6 +17,10 @@ ForeignKey: for relationships between models (e.g., one-to-many)
 ManyToManyField: for many-to-many relationships
 OneToOneField: for one-to-one relationships 
 '''
+def image_upload(instance, filename):
+    imagename, extention = filename.split('.')
+    return "jobs/%s.%s" % (instance.id,extention)
+
 JOB_TYPE = (
     ('full-time', 'Full Time'),
     ('part-time', 'Part Time')
@@ -34,10 +38,11 @@ class Job(models.Model): #table
     # location = models.CharField(max_length=100)
     salary = models.IntegerField(default=0)
     experience = models.IntegerField(default=1)
-    # company = models.CharField(max_length=100)
     published_at = models.DateTimeField(auto_now=True)
     vacancy = models.IntegerField(default=1)
     category = models.ForeignKey('Category', on_delete=models.CASCADE, null=True)
+    image = models.ImageField(upload_to=image_upload, null=True , blank=True)
+    
     
     def __str__(self):
         return self.title
